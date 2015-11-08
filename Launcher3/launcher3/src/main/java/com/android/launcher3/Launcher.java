01b789882b98f7844f781e1184e0d46ee8aa4737
liyujiang <liyujiang_tk@yeah.net>
2015-11-08 20:09:15 +0800

    整理

--- Launcher3/launcher3/src/main/java/com/android/launcher3/Launcher.java
+++ Launcher3/launcher3/src/main/java/com/android/launcher3/Launcher.java
@@ -161,8 +161,8 @@ public class Launcher extends Activity
 
     static final String EXTRA_SHORTCUT_DUPLICATE = "duplicate";
 
-    static final int SCREEN_COUNT = 5;
-    static final int DEFAULT_SCREEN = 2;
+    static final int SCREEN_COUNT = 3;
+    static final int DEFAULT_SCREEN = 1;
 
     // To turn on these properties, type
     // adb shell setprop log.tag.PROPERTY_NAME [VERBOSE | SUPPRESS]
